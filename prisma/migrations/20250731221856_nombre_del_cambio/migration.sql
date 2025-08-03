fb9d7cf951c35937f61b4409b1efde7f1e800081
DAVIDKNO2-2 <hectordavidosoriocano@gmail.com>
2025-08-02 22:39:56 -0500

    Subiendo módulo a la rama entrenamientosszs

---
+++ prisma/migrations/20250731221856_nombre_del_cambio/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "public"."Routine" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Routine_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Exercise" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "repetitions" TEXT,
+    "instructions" TEXT,
+    "routineId" INTEGER NOT NULL,
+
+    CONSTRAINT "Exercise_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."Exercise" ADD CONSTRAINT "Exercise_routineId_fkey" FOREIGN KEY ("routineId") REFERENCES "public"."Routine"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const apiRoutes = require('./routes/api');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3087;
 
 // Middleware
 // Use body-parser to parse JSON bodies
